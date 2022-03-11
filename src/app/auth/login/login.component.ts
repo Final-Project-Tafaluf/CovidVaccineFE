7aff716da8a41be41e43c8a268438c108138b525
unknown <amjaddeeb66@gmail.com>
2022-03-12 01:48:53 +0300

    task

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -10,13 +10,23 @@ import { NgxSpinnerService } from 'ngx-spinner';
 })
 export class LoginComponent implements OnInit {
 
-  email = new FormControl('',[Validators.required,Validators.email]);
-  password = new FormControl('',[Validators.required,Validators.minLength(8)]);
+  email = new FormControl(localStorage.getItem("email"),[Validators.required,Validators.email]);
+  password = new FormControl(localStorage.getItem("password"),[Validators.required,Validators.minLength(8)]);
+  fdsds = localStorage.getItem("isChecked") ? true : false;
+  isChecked =  new FormControl(this.fdsds);
   constructor(private spinner :NgxSpinnerService, private router:Router) {}
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    // this.email = new FormControl() localStorage.getItem("email");
+  }
+
   Onsubmit(){
     console.log(this.email.value);
     console.log(this.password.value);
+
+    localStorage.setItem("email" , this.email.value);
+    localStorage.setItem("password" , this.password.value);
+    localStorage.setItem("isChecked",JSON.stringify(this.isChecked.value));
+
     this.spinner.show();
     setTimeout(() => {
       this.spinner.hide();
