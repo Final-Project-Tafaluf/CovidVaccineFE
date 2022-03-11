ac62ee7652285440023ffa8e95506a5a91633def
obaid <eng.abdallahobaid@gmail.com>
2022-03-12 02:02:18 +0300

    amjad

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -12,21 +12,28 @@ export class LoginComponent implements OnInit {
 
   email = new FormControl(localStorage.getItem("email"),[Validators.required,Validators.email]);
   password = new FormControl(localStorage.getItem("password"),[Validators.required,Validators.minLength(8)]);
-  fdsds = localStorage.getItem("isChecked") ? true : false;
-  isChecked =  new FormControl(this.fdsds);
+  isCheckedLocal = localStorage.getItem("isChecked") == "true" ? true : false;
+  isChecked =  new FormControl(this.isCheckedLocal);
   constructor(private spinner :NgxSpinnerService, private router:Router) {}
   ngOnInit(): void {
     // this.email = new FormControl() localStorage.getItem("email");
+    console.log("this.isCheckedLocal",this.isCheckedLocal)
+    console.log("this.isChecked",this.isChecked)
+
   }
 
   Onsubmit(){
     console.log(this.email.value);
     console.log(this.password.value);
-
+    if( this.isChecked.value==true){
     localStorage.setItem("email" , this.email.value);
     localStorage.setItem("password" , this.password.value);
     localStorage.setItem("isChecked",JSON.stringify(this.isChecked.value));
-
+    }else{
+      localStorage.setItem("email" , "");
+      localStorage.setItem("password" , "");
+      localStorage.setItem("isChecked",JSON.stringify(this.isChecked.value));
+    }
     this.spinner.show();
     setTimeout(() => {
       this.spinner.hide();
