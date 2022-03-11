7b12e7b9a49d31ec4f6b35d0e3adf21fcde76f7a
obaid <eng.abdallahobaid@gmail.com>
2022-03-12 01:10:34 +0300

    fix login

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -6,7 +6,7 @@ import { NgxSpinnerService } from 'ngx-spinner';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.sass']
+  styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
 
