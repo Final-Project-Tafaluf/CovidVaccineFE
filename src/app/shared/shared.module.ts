de97a770f3bd778cbcac2541c0a0a8aec11495a2
AshrafAwwad96 <98114301+AshrafAwwad96@users.noreply.github.com>
2022-03-24 01:10:12 +0300

    edits on homePage navBar tistimonal contact us

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -6,14 +6,9 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatInputModule} from '@angular/material/input';
 import { RouterModule } from '@angular/router';
-<<<<<<< HEAD
 import {MatDialogModule} from '@angular/material/dialog';
 import {MatButtonModule} from '@angular/material/button';
-=======
-import {MatButtonModule} from '@angular/material/button';
-import {MatDialogModule} from '@angular/material/dialog';
 import { CustomeDatePipe } from '../Pipes/custome-date.pipe';
->>>>>>> 095ceb037e8652ce257b608a844c526b0d7d4da9
 
 
 
@@ -32,13 +27,9 @@ import { CustomeDatePipe } from '../Pipes/custome-date.pipe';
     FormsModule,
     ReactiveFormsModule,
     RouterModule,
-<<<<<<< HEAD
     MatDialogModule,
     MatButtonModule
-=======
-    MatButtonModule,
-    MatDialogModule
->>>>>>> 095ceb037e8652ce257b608a844c526b0d7d4da9
+   
   ],
   exports:[
     MatFormFieldModule,
@@ -47,14 +38,9 @@ import { CustomeDatePipe } from '../Pipes/custome-date.pipe';
     ReactiveFormsModule,
     HeaderComponent,
     FooterComponent,
-<<<<<<< HEAD
-    MatDialogModule,
-    MatButtonModule
-=======
     MatButtonModule,
     MatDialogModule,
     CustomeDatePipe
->>>>>>> 095ceb037e8652ce257b608a844c526b0d7d4da9
   ]
 })
 export class SharedModule { }
