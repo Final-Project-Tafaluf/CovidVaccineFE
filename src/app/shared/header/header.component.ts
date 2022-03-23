ddb27b453efb883323f1767631fe7b15dce598f5
obaid <eng.abdallahobaid@gmail.com>
2022-03-23 20:53:40 +0300

    change logo

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -6,10 +6,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
+  activeObj:{active_home:string,active_about:string,active_testimonial:string,active_contact:string,active_service:string,active_register:string,active_login:string}={
+    active_home:"active",
+    active_about:"",
+    active_testimonial:"",
+    active_contact:"",
+    active_service:"",
+    active_register:"",
+    active_login:"",
+  };
 
   constructor() { }
 
   ngOnInit(): void {
-  }
 
+  }
+  activeNav(value :string){
+  debugger
+   for (let key in this.activeObj) {
+    debugger
+    if(key ==value){
+      //this.activeObj[key] ="active";
+    }else{
+      //this.activeObj[key]  ="";
+    }
+    console.log(this.activeObj)
+   }
+  }
 }
