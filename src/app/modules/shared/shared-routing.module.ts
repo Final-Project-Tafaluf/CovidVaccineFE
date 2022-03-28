8be702df6010d293cfa596b5dc7dd354ae5dc07f
AshrafAwwad96 <98114301+AshrafAwwad96@users.noreply.github.com>
2022-03-28 20:54:06 +0300

    mission & vision

--- src/app/modules/shared/shared-routing.module.ts
+++ src/app/modules/shared/shared-routing.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AboutusComponent } from '../components/aboutus/aboutus.component';
+import { AboutusComponent } from 'src/app/components/aboutus/aboutus.component';
+import { ContactusComponent } from 'src/app/components/contactus/contactus.component';
+import { HomeComponent } from 'src/app/components/home/home.component';
+
 import { AdminModule } from '../admin/admin.module';
 import { AuthModule } from '../auth/auth.module';
-import { ContactusComponent } from '../components/contactus/contactus.component';
-import { HomeComponent } from '../components/home/home.component';
+
+
 
 const routes: Routes = [
   {
