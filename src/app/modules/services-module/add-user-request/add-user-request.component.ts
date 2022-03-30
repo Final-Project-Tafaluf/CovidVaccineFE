11f46831e5d217a3d2d90782efb3aa29426b5a52
obaid <eng.abdallahobaid@gmail.com>
2022-03-30 20:19:17 +0300

    request vaccine

--- src/app/modules/services-module/add-user-request/add-user-request.component.ts
+++ src/app/modules/services-module/add-user-request/add-user-request.component.ts
@@ -1,4 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import Map from 'ol/Map';
+import View from 'ol/View';
+import TileLayer from 'ol/layer/Tile';
+import OSM from 'ol/source/OSM';
+import Style from 'ol/style/Style';
+import Icon from 'ol/style/Icon';
+import { Feature } from 'ol';
+import { Point } from 'ol/geom';
+import VectorSource from 'ol/source/Vector';
+import VectorLayer from 'ol/layer/Vector';
 
 @Component({
   selector: 'app-add-user-request',
@@ -6,10 +17,65 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-user-request.component.scss']
 })
 export class AddUserRequestComponent implements OnInit {
-
+  CreateForm:FormGroup=new FormGroup({
+    HEALTH_CENTER: new FormControl("",[Validators.required]),
+    VACCINE_TYPE: new FormControl("",[Validators.required]),
+    USER_ID: new FormControl("1",[Validators.required]),
+    REQUEST_DATE:new FormControl("",[Validators.required]),
+  })
+  map: Map | undefined;
   constructor() { }
 
   ngOnInit(): void {
+
+    const vectorSource = new VectorSource({
+    });
+    for (let i = 0; i < 10; i++) {
+      const iconFeature = new Feature({
+        geometry: new Point([39.2384+i,31.5852+i]),
+        name: 'Null Island',
+        population: 4000,
+        rainfall: 500,
+      });
+      const iconStyle = new Style({
+        image: new Icon({
+          anchor: [0.5, 46],
+          anchorXUnits: 'fraction',
+          anchorYUnits: 'pixels',
+          src: '../../../../assets/life_care/images/icon-logo.png',
+        }),
+      });
+      
+      iconFeature.setStyle(iconStyle);
+      vectorSource.addFeatures([iconFeature])
+    }
+
+    
+
+    
+    const vectorLayer = new VectorLayer({
+      source: vectorSource,
+    });
+    
+    
+
+    this.map = new Map({
+      view: new View({
+        projection: 'EPSG:4326',
+        center: [39.2384,31.5852 ],
+        zoom: 1,
+      }),
+      layers: [
+        new TileLayer({
+          source: new OSM(),
+        }),vectorLayer
+      ],
+      target: 'ol-map'
+    });
+
   }
 
+  sendRequest(){
+
+  }
 }
