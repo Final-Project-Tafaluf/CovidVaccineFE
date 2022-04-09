0f1e8fea1740c9e52193ad56f2afe9e65370998a
AshrafAwwad96 <98114301+AshrafAwwad96@users.noreply.github.com>
2022-04-10 00:26:15 +0300

    certificate download  pdf

--- src/app/modules/client/certificate/certificate.component.ts
+++ src/app/modules/client/certificate/certificate.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import jsPDF from 'jspdf';
 import { LocalStorageService } from 'src/app/Services/local-storage.service';
 import { CertificateRestService } from 'src/app/Services/rest/certificate-rest.service';
 
@@ -8,9 +9,18 @@ import { CertificateRestService } from 'src/app/Services/rest/certificate-rest.s
   styleUrls: ['./certificate.component.scss']
 })
 export class CertificateComponent implements OnInit {
-
+  @ViewChild('content', { static: false }) el!: ElementRef;
   constructor(private localStorageService:LocalStorageService,public certificateRestService:CertificateRestService) { }
+  title = 'angularpdfgenerator';
 
+  makePdf() {
+    let pdf = new jsPDF()
+    pdf.html(this.el.nativeElement, {
+      callback: (pdf) => {
+        pdf.save("Certificate.pdf")
+      }
+    })
+  }
   ngOnInit(): void {
     var token = this.localStorageService.getToken();
     var tokenData: any = this.localStorageService.tokenDecode(token);
