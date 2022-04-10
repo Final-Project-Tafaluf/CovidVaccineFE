e3c9175113d8b70461944ba10c418e03c8907a7f
obaid <eng.abdallahobaid@gmail.com>
2022-04-10 06:40:07 +0300

    schedule mod

--- src/app/modules/services-module/schedule/schedule.component.ts
+++ src/app/modules/services-module/schedule/schedule.component.ts
@@ -11,20 +11,36 @@ import { ScheduleRestService } from 'src/app/Services/rest/schedule-rest.service
 export class ScheduleComponent implements OnInit {
   userRequests:any;
   userData:any;
+  userSchedules:any;
   constructor(public scheduleRestService: ScheduleRestService,private localStorageService: LocalStorageService) { }
 
   ngOnInit(): void {
-    this.getUserRequests()
+    this.getUserDataFromLocal();
+    this.getUserRequests();
+    this.getUserSchedules();
   }
-  async getUserRequests(){
+  getUserDataFromLocal(){
     var token = this.localStorageService.getToken();
     if (token) {
-      debugger;
+      // debugger;
       this.userData = this.localStorageService.tokenDecode(token);
-      this.userRequests = await this.scheduleRestService.getUserRequest(this.userData.nameid);
     }
   }
-  removeRequest(){
 
+  async getUserRequests(){
+    // debugger;
+      this.userRequests = await this.scheduleRestService.getUserRequest(this.userData.nameid);
+  }
+
+  async getUserSchedules(){
+    debugger;
+    this.userSchedules = await this.scheduleRestService.getScheduleByUserId(this.userData.nameid);
+    console.log("this.userSchedules",this.userSchedules);
+  }
+
+  removeRequest(requestId: number){
+    debugger;
+    this.scheduleRestService.deleteUserRequestById(requestId);
+    this.getUserRequests(); // To refresh the table
   }
 }
