43917383c457016e03c9860e1fb37cf3ee031f67
unknown <mohammadbeshtawi99@gmail.com>
2022-04-17 08:48:02 +0300

    manage schedual

--- src/app/modules/admin/manage-schedules/manage-schedules.component.ts
+++ src/app/modules/admin/manage-schedules/manage-schedules.component.ts
@@ -3,6 +3,7 @@ import { FormControl, FormGroup } from '@angular/forms';
 import { MatDialog } from '@angular/material/dialog';
 import { ManageHealthCenterRestService } from 'src/app/Services/rest/manage-health-center-rest.service';
 import { ManageScheduleRestService } from 'src/app/Services/rest/manage-schedule-rest.service';
+import { ManageVaccineRestService } from 'src/app/Services/rest/manage-vaccine-rest.service';
 
 @Component({
   selector: 'app-manage-schedules',
@@ -24,6 +25,8 @@ export class ManageSchedulesComponent implements OnInit {
     center_Id:new FormControl(),
     user_Id:new FormControl(),
     doctor_name:new FormControl(),
+    vaccine_id:new FormControl(),
+    dose_taken_date:new FormControl()
 
   })
 
@@ -35,16 +38,22 @@ export class ManageSchedulesComponent implements OnInit {
     end_Time:new FormControl(),
     center_Id:new FormControl(),
     user_Id:new FormControl(),
-    doctor_name:new FormControl()
+    doctor_name:new FormControl(),
+    vaccine_id:new FormControl(),
+    dose_taken_date:new FormControl()
   })
 
   schedule : any ={}
 
-  constructor(private dialog:MatDialog,public manageScheduleRestService:ManageScheduleRestService,public manageHealthCenterRestService:ManageHealthCenterRestService) { }
+  constructor(private dialog:MatDialog,
+    public manageScheduleRestService:ManageScheduleRestService,
+    public manageHealthCenterRestService:ManageHealthCenterRestService,
+    public manageVaccineRestService:ManageVaccineRestService) { }
 
   ngOnInit(): void {
     this.manageScheduleRestService.getAll();
     this.manageHealthCenterRestService.getAll();
+    this.manageVaccineRestService.getAll();
   }
 
   save(){
@@ -71,7 +80,9 @@ debugger
         end_Time:data.end_Time,
         center_Id:data.center_Id,
         user_Id:data.user_Id,
-        doctor_name:data.doctor_name
+        doctor_name:data.doctor_name,
+        vaccine_id:data.vaccine_id,
+        dose_taken_date:data.dose_taken_date
     }
     this.UpdateForm.controls['id'].setValue(this.schedule.id)
     this.UpdateForm.controls['user_Id'].setValue(this.schedule.user_Id)
