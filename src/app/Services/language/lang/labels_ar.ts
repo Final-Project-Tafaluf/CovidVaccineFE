5186f6e79d28e025f85585633f4dfd85647ea1cb
AshrafAwwad96 <98114301+AshrafAwwad96@users.noreply.github.com>
2022-04-22 15:56:47 +0300

    testimonial yes no pipe

--- src/app/Services/language/lang/labels_ar.ts
+++ src/app/Services/language/lang/labels_ar.ts
@@ -5,4 +5,26 @@ export const arabic = {
   edit: "تعديل",
   description: "الوصف",
   dashboard_analytics:"تحليل البيانات",
+  users:"المستخدمون",
+  admin:"المدراء",
+  healthcenter:"المراكز الصحية",
+  mails:"رسائل البريد",
+  totalusers:"مجموع المستخدمين",
+  totaldosetaken:"إجمالي الجرعة المأخوذة",
+  dashboardAnalytics:"لوحة القيادة",
+  absent:"غائب",
+  requests:"الطلبات",
+  firstdose:"جرعة أولى",
+  seconddose:"الجرعة الثانية",
+  thirddose:"الجرعة الثالثة",
+  totalVaccineDose:"مجموع اللقاح",
+  totalnovdtci:"إجمالي عدد جرعة اللقاح الواردة ",
+  centerName:"اسم المركز",
+  vaccineType:"نوع اللقاح",
+  NofDose:"عدد الجرع",
+  expireDate:"تاريخ انتهاء الصلاحية",
+  healthcenters:"المراكز الصحية",
+
+  
+
   };
--- src/app/Services/language/lang/labels_en.ts
+++ src/app/Services/language/lang/labels_en.ts
@@ -5,4 +5,26 @@ export const english = {
   edit: "Edit",
   description: "Description",
   dashboard_analytics:"Dashboard analytics",
+  users:"USERS",
+  admin:"ADMINS",
+  healthcenter:"HEALTH CENTERS",
+  mails:"MAILS",
+  totalusers:"Total Users",
+  totaldosetaken:"Total Dose Taken",
+  dashboardAnalytics:"Dashboard Analytics",
+  absent:"ABSENT",
+  requests:"REQUESTS",
+  firstdose:"FIRST DOSE",
+  seconddose:"SECOND DOSE",
+  thirddose:"THIRD DOSE",
+  totalVaccineDose:"Total Vaccine Dose",
+  totalnovdtci:"Total number of Vaccine Dose that come in",
+  centerName:"CENTER NAME",
+  vaccineType:"Vaccine Type",
+  NofDose:"# Of Dose",
+  expireDate:"Expire Date",
+  healthcenters:"Health Centers",
+
+
+
 };
