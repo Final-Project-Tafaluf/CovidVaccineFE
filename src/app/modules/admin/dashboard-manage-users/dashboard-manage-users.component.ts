17470e139317e764c85b2249e8f725ca368a9245
Amjad Altheeb <amjad.altheeb@info-seed.com>
2022-04-25 20:44:20 +0300

    manage user

--- src/app/modules/admin/dashboard-manage-users/dashboard-manage-users.component.ts
+++ src/app/modules/admin/dashboard-manage-users/dashboard-manage-users.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, TemplateRef, ViewChild } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { MatDialog } from '@angular/material/dialog';
 import { ManageUsersRestService } from 'src/app/Services/rest/manage-users-rest.service';
 
@@ -14,7 +14,7 @@ export class DashboardManageUsersComponent implements OnInit {
   @ViewChild('callDeleteDialog') callDeleteDialog! :TemplateRef<any>
   @ViewChild('callDeleteDialog') callDetailsDialog! :TemplateRef<any>
 
-  user:any={}
+  userProfile:any={}
   userInfo:any={}
   first_Name:any=''
   info:any = ''
@@ -27,49 +27,30 @@ export class DashboardManageUsersComponent implements OnInit {
 
   }
 
-  /* inputValue(ev:any){
-    this.first_Name=ev.target.value;
-    console.log(ev.target.value);
-  } */
 
-  /* search(){
-    // debugger;
-    this.manageUsersRestService.getbyName(this.first_Name.toString());
-    } */
+  CreateForm: FormGroup = new FormGroup({
+    first_Name: new FormControl('', [Validators.required]),
+    last_Name: new FormControl('', [Validators.required]),
+    ssn: new FormControl('', [Validators.required]),
+    gender: new FormControl(),
+    birthdate: new FormControl(),
+    address: new FormControl(),
+    image: new FormControl(),
+    phone: new FormControl(),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [
+      Validators.required,
+      Validators.minLength(8),
+    ]),
+    confirm_Password: new FormControl('', [
+      Validators.required,
+      Validators.minLength(8),
+    ]),
+    username: new FormControl('', Validators.required),
+    role: new FormControl(),
+  });
 
-    CreateForm :FormGroup =new FormGroup
-  (
-    {
-    first_Name:new FormControl(),
-    last_Name:new FormControl(),
-    ssn:new FormControl(),
-    gender:new FormControl(),
-    birthdate:new FormControl(),
-    address:new FormControl(),
-    image:new FormControl(),
-    phone:new FormControl(),
-    email:new FormControl(),
-    password:new FormControl(),
-    username:new FormControl(),
-    role:new FormControl(),
-    }
-  )
 
-  /* CreateForm :FormGroup =new FormGroup
-  (
-    {
-      ssn:new FormControl(),
-      first_Name:new FormControl(),
-      last_Name:new FormControl(),
-      gender:new FormControl(),
-      birthdate:new FormControl(),
-      address:new FormControl(),
-      phone:new FormControl(),
-      email:new FormControl(),
-      image:new FormControl(),
-      username:new FormControl()
-    }
-  ) */
   openCreateDialog(){
     this.dialog.open(this.callCreateDialog)
   }
@@ -83,24 +64,25 @@ export class DashboardManageUsersComponent implements OnInit {
 
   UpdateForm:FormGroup=new FormGroup({
     id:new FormControl(),
-    first_Name: new FormControl(),
-    last_Name: new FormControl(),
+    first_Name: new FormControl('',Validators.required),
+    last_Name: new FormControl('',Validators.required),
     ssn: new FormControl(),
-    gender:new FormControl(),
-    birthdate:new FormControl(),
-    address:new FormControl(),
-    phone:new FormControl(),
+    gender:new FormControl('',Validators.required),
+    birthdate:new FormControl('',Validators.required),
+    address:new FormControl('',Validators.required),
+    phone:new FormControl('',Validators.required),
     image:new FormControl(),
     email:new FormControl(),
     password:new FormControl(),
     username:new FormControl(),
-    dose_Number : new FormControl(),
-    role_Id : new FormControl(),
+    role_Id:new FormControl()
   })
+  defaultPass: string = '';
+
   openUpdateDialog(
     id1 :any,first_Name1 :any,last_Name1 :any,ssn1 :any,gender1:any,birthdate1:any,
-    address1 :any,phone1:any,email1:any,password1:any,username1:any,image1:any,dose:any,role:any){
-      this.user={
+    address1 :any,phone1:any,email1:any,password1:any,username1:any,image1:any,role1:any){
+      this.userProfile={
       id:id1,
       first_Name:first_Name1,
       last_Name:last_Name1,
@@ -113,21 +95,22 @@ export class DashboardManageUsersComponent implements OnInit {
       password:password1,
       username:username1,
       image:image1,
-      dose_Number:dose,
-      role_Id : role
+      role_Id:role1
     }
-    this.UpdateForm.controls['id'].setValue(this.user.id)
-    this.UpdateForm.controls['ssn'].setValue(this.user.ssn)
-    this.UpdateForm.controls['email'].setValue(this.user.email)
-    this.UpdateForm.controls['password'].setValue(this.user.email)
-    this.UpdateForm.controls['role_Id'].setValue(this.user.role_Id)
     // debugger
-    this.dialog.open(this.callUpdateDialog)
+    this.UpdateForm.controls['id'].setValue(this.userProfile.id)
+    this.UpdateForm.controls['ssn'].setValue(this.userProfile.ssn)
+    this.UpdateForm.controls['email'].setValue(this.userProfile.email)
+    this.UpdateForm.controls['username'].setValue(this.userProfile.username)
+    this.UpdateForm.controls['role_Id'].setValue(this.userProfile.role_Id)
+    this.dialog.open(this.callUpdateDialog,{
+      panelClass: 'dialog-container-custom'
+    });
   }
 
   updateUser(){
     this.manageUsersRestService.updateUser(this.UpdateForm.value);
-    console.log(this.user);
+    console.log(this.userProfile);
     /* window.location.reload(); */
   }
 
@@ -165,8 +148,21 @@ export class DashboardManageUsersComponent implements OnInit {
     this.info=ev.target.value;
   }
   search(){
- 
+
     this.manageUsersRestService.searchUser(this.info);
   }
 
+  onChange() {
+    if (
+      this.CreateForm.controls['password'].value ==
+      this.CreateForm.controls['confirm_Password'].value
+    )
+      this.CreateForm.controls['confirconfirm_Passwordmpassword'].setErrors(
+        null
+      );
+    else
+      this.CreateForm.controls['confirm_Password'].setErrors({
+        mismatch: true,
+      });
+  }
 }
